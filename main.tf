69453ab1205eeacd034874cc2d2c886487145315
abhishek-prog7 <72608278+abhishek-prog7@users.noreply.github.com>
2023-11-23 15:22:03 +0530

    fix errors

--- main.tf
+++ main.tf
@@ -15,12 +15,11 @@ data "aws_ami" "app_ami" {
 }
 
 resource "aws_instance" "blog" {
- count = 2
-
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.blog.id]
+  
   tags = {
     Name = "HelloWorld"
   }
@@ -30,13 +29,13 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_security_group" "blog"{
+resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in and allow everything out"
   vpc_id      = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in"{
+resource "aws_security_group_rule" "blog_http_in" {
   type = "ingress"
   from_port = 80
   to_port = 80
@@ -46,7 +45,17 @@ resource "aws_security_group_rule" "blog_http_in"{
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_http_out"{
+resource "aws_security_group_rule" "blog_https_in" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
   type        =  "egress"
   from_port   = 0
   to_port     =   0
